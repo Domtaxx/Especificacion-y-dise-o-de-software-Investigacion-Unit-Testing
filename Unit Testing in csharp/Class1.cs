9fec44b5bdce94f2f58d3a0a67fb23590f6ec32f
Brian Wagemans <briwag88@estudaintec.cr>
2022-03-12 23:24:48 -0600

    starting clases

---
+++ Unit Testing in csharp/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_Testing_in_csharp
+{
+    class Calculator
+    {
+        public double Sum(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public double Sum(List<double> numbers_to_add)
+        {
+            double result = 0;
+            for(int i = 0; i < numbers_to_add.Count; i++)
+            {
+                result = Sum(result,numbers_to_add[i]);
+            }
+            return result;
+        }
+        public double Subtraction(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public double Subtraccion(List<double> numbers_to_add)
+        {
+            double result = 0;
+            for (int i = 0; i < numbers_to_add.Count; i++)
+            {
+                result = Subtraction(result, numbers_to_add[i]);
+            }
+            return result;
+        }
+
+        public double division(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+        public double multiplicacion(double num1, double num2) { 
+            return num1 * num2;
+        }
+    }
+}
---
+++ Unit Testing in csharp/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace Unit_Testing_in_csharp
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.
+            Assert.Pass();
+        }
+    }
+}
