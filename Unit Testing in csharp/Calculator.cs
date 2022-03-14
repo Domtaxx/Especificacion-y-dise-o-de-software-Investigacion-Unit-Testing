977773135158440e3cfc33321c936ac4c8e08fc3
Brian Wagemans <briwag88@estudiantec.cr>
2022-03-14 01:04:09 -0500

    Advances in testing

---
+++ Unit Testing in csharp/Calculator.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_Testing_in_csharp
+{
+    class Calculator
+    {
+        public double Sum(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public double Sum(List<double> numbers_to_add)
+        {
+            double result = 0;
+            for(int i = 0; i < numbers_to_add.Count; i++)
+            {
+                result = Sum(result,numbers_to_add[i]);
+            }
+            return result;
+        }
+        public double Sum_wrong(List<double> numbers_to_add)
+        {
+            double result = 0;
+            for (int i = 0; i < numbers_to_add.Count; i++)
+            {
+                result = Sum(result, numbers_to_add[1]);
+            }
+            return result;
+        }
+
+        public double Subtraction(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public double Subtraccion(List<double> numbers_to_add)
+        {
+            double result = 0;
+            for (int i = 0; i < numbers_to_add.Count; i++)
+            {
+                result = Subtraction(result, numbers_to_add[i]);
+            }
+            return result;
+        }
+        public double Subtraccion_wrong(List<double> numbers_to_add)
+        {
+            double result = 0;
+            for (int i = 0; i < numbers_to_add.Count; i++)
+            {
+                result = Subtraction(numbers_to_add[i], result);
+            }
+            return result;
+        }
+
+        public double division(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+        public double multiplicacion(double num1, double num2) { 
+            return num1 * num2;
+        }
+    }
+}
--- Unit Testing in csharp/UnitTest1.cs
+++ Unit Testing in csharp/UnitTest1.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace Unit_Testing_in_csharp
 {
@@ -7,13 +8,33 @@ namespace Unit_Testing_in_csharp
         [SetUp]
         public void Setup()
         {
+
+            Calculator calc = new Calculator();
         }
 
         [Test]
-        public void Test1()
+        public void Test_sum_op()
         {
-            Assert.
-            Assert.Pass();
+            Calculator calc = new Calculator();
+            
+            Assert.AreEqual(4, calc.Sum(1, 3));
         }
+        /*
+        [Test]
+        public void Test_add_all_list()
+        {
+            Calculator calc = new Calculator();
+            List<double> list = new List<double>();
+            list.Add(1);
+            list.Add(10);
+            list.Add(4);
+            list.Add(3);
+            //Using an assert to test the wrong output
+            Assert.AreEqual(18, calc.Sum_wrong(list));
+
+            //Using an assert to test the correct output
+            Assert.AreEqual(18, calc.Sum(list));
+        }
+        */
     }
 }
